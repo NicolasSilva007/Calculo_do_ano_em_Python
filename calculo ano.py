8ed48c86a801cddefe2ab729fa09d8f8b58a7f51
Nicolas Silva Santos <141461479+NicolasSilva007@users.noreply.github.com>
2023-08-11 22:10:33 -0300

    Add files via upload

---
+++ calculo ano.py
@@ -0,0 +1,12 @@
+nome = input("Digite o nome: ")
+idade = int(input("Digite idade:"))
+ano = int(input("Ano de nascimento: "))
+saldo = float(input("Digite seu saldo: "))
+percen = int(input("Digite o percentual: "))
+
+print (nome,"seu saldo é: ",saldo)
+print ("Seu saldo com mais",percen,"%:",(saldo * percen)/100+saldo)
+
+print ("--------------------------------------------------------------------")
+print (f"Você {nome} que nasceu em {ano} tera um saldo de {percen}%, que sera de R$ {(saldo*percen)/100+saldo}")
+print ("--------------------------------------------------------------------")
